4711f741b437c51aa39b32a38ab913baff55bcd5
PhilipAUT <37860925+PhilipAUT@users.noreply.github.com>
2018-04-15 16:59:58 +1200

    Generic Map

--- app/src/main/java/com/aut/sdp2018/autuniapp/MainMenu.java
+++ app/src/main/java/com/aut/sdp2018/autuniapp/MainMenu.java
@@ -1,13 +1,23 @@
 package com.aut.sdp2018.autuniapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void openMapActivity(View view)
+    {
+        Intent autMap = new Intent(this,AUTMap.class);
+        startActivity(autMap);
     }
 }
