40fcccde8a7a7fca18a95e9647c23b3a64ee9bb3
the65VV <srividz97@gmail.com>
2018-04-26 11:20:04 +1200

    Added settings

--- app/src/main/java/com/aut/sdp2018/autuniapp/MainMenu.java
+++ app/src/main/java/com/aut/sdp2018/autuniapp/MainMenu.java
@@ -3,6 +3,9 @@ package com.aut.sdp2018.autuniapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainMenu extends AppCompatActivity
@@ -19,5 +22,25 @@ public class MainMenu extends AppCompatActivity
     {
         Intent autMap = new Intent(this,AUTMap.class);
         startActivity(autMap);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenusettings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent settings = new Intent(this, SettingsActivity.class);
+                startActivity(settings);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
