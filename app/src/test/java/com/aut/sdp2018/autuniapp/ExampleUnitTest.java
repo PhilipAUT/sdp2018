63ffdb413e73edd8ffa7fefb49d94c749b73b75f
the65VV <srividz97@gmail.com>
2018-05-04 12:11:54 +1200

    added comin soon labels and commented code

--- app/src/test/java/com/aut/sdp2018/autuniapp/ExampleUnitTest.java
+++ app/src/test/java/com/aut/sdp2018/autuniapp/ExampleUnitTest.java
@@ -10,8 +10,19 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+
+
+//    @Test
+//    public void isClicked(){
+//
+//        assertEquals(autuniapp.MainMenu, autuniapp.MainMenu);
+//    }
+
+
 }
