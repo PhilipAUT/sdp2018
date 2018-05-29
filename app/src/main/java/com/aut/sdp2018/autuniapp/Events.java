c2976470d990e60e2af2f04800f4c3397bcd1c97
PhilipAUT <37860925+PhilipAUT@users.noreply.github.com>
2018-05-29 16:15:09 +1200

    events work -

--- app/src/main/java/com/aut/sdp2018/autuniapp/Events.java
+++ app/src/main/java/com/aut/sdp2018/autuniapp/Events.java
@@ -15,7 +15,7 @@ public class Events extends ListActivity {
         System.out.print("Events Opened\n");
 
 
-        String[] eventsArray ={"Event 1","Event 2","Event 3","Event 4","Event 5","Event 6"};
+        String[] eventsArray ={"Event 1- Tech Week","Event 2, 4/6/18- NZSL Signing Coffee ","Event 3, 4/6/18 - Ocha No Jikan ","Event 4, 5/6/18 - Visiting Writers Programme: Deborah Shepard","Event 5, 6/6/18 - Uni101- My future","Event 6 , 22/6/18 - Creative Technologies: Mid-year show of student work"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,eventsArray);
         setListAdapter(adapter);
 
